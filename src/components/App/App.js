bd3a9fe3c118a6dcc053f6d91f57efa35ae80bd5
Megan Teh <ywtm@protonmail.com>
2021-04-14 22:29:29 +1200

    Add login function

---
+++ src/components/App/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Result from '../Result/Result';
+import useToken from './useToken';
+
+function App() {
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>SEEDS</h1>
+      <p>Login successful!</p>
+      <p>
+        Login details will be stored in same session when you navigate
+        to different pages
+      </p>
+      <p>
+        Pages available: /dashboard and /result
+      </p>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/result">
+            <Result />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
