a57863895f3fe5944336b94a2dbe81b7191306da
Megan Teh <ywtm@protonmail.com>
2021-04-15 14:30:02 +1200

    Add passing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 /* import { render, screen } from '@testing-library/react';
-import App from './components/App/App';
+import App from './components/App/App'; */
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
- */
+}); */
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  })
+})
