b3e697a66403bcd6d0b271457e7f433fc3f21cf0
Wa11bang <waldo@theron.co.nz>
2021-04-27 13:51:53 +1200

    feat: repo api now includes GET and POST request handling for articles

--- backend/routes/api/Repo.js
+++ backend/routes/api/Repo.js
@@ -1,24 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-// need Evidence model
+// db models
+const Evidence = require('../../models/Evidence');
 
 // @route GET api/repo
 // @description get all articles
 router.get('/', (req, res) => {
-    res.send({
-        token: '{}',
-    });
+    Evidence.find()
+        .then(function(articles){ 
+            if(articles == null)
+            {
+                throw new Error("No Articles found");
+                
+            }
+            const result = { success: 1 , result: articles };
+            res.json(result)
+        })
+        .catch(err => res.status(404).json({ suceess: 0, msg: err.message }));
 });
 
 // @route GET api/repo/:article_id
 // @description get single article
 router.get('/:article_id', (req, res) => {
-    res.send({
-        token: req.params.article_id,
-    });
+    Evidence.findById(req.params.article_id)
+        .then(function(article){ 
+            if(article == null)
+            {
+                throw new Error("Article does not exist");
+            }
+            const result = { success: 1 , result: article };
+            res.json(result)
+        })
+        .catch(err => res.status(404).json({ success: 0, msg: err.message }));
+});
 
-    // Evidence find by ID here.
+// @route POST api/repo/create
+// @description adds a new article to the database 
+// (no verification / no validation)
+router.post('/create', (req, res) => {
+    Evidence.create(req.body)
+      .then(article => res.json({ success: 1, msg: 'Article added successfully' }))
+      .catch(err => res.status(400).json({ success: 0, msg: 'Unable to add this Article' }));
 });
 
 module.exports = router;
