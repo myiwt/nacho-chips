6f4007abd436e56b5b66da1d6c9845b71dcafc81
Wa11bang <waldo@theron.co.nz>
2021-04-27 19:13:48 +1200

    test: added basic passing unit-test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders check SEEDS title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SEEDS/i);
   expect(linkElement).toBeInTheDocument();
 });
