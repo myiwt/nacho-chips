686f1dcf897e3b1fb0093830ae77cee170d3a358
bnort <brett.d.norton@gmail.com>
2021-04-28 14:07:57 +1200

    feat: added search and search bar functionality

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,34 @@
+//import { useHistory } from 'react-router-dom';
+
+const SearchBar = ({ searchQuery, setSearchQuery }) => {    
+    /*const history = useHistory();
+    const onSubmit = (e) => {
+        history.push(`?s=${searchQuery}`);
+        e.preventDefault();
+    };*/
+
+    return (
+    <form 
+        action="/view-all" 
+        method="get"
+        autoComplete="off"
+        //onSubmit={onSubmit}
+    >
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search repository</span>
+        </label>
+        <input 
+            //value={searchQuery}
+            //onInput={(e) => setSearchQuery(e.target.value)}
+            type="text"
+            id="header-search"
+            placeholder="Search for evidence"
+            name="s"
+            size="50"
+        />
+        <button type="submit">Search</button>
+    </form>
+    );
+};
+
+export default SearchBar;
