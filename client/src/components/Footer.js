13ef631f8854392e7091b4636c91622c1392f82a
Megan Teh <ywtm@protonmail.com>
2021-05-10 10:54:27 +1200

    refactor: create components for footer and nav

---
+++ client/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import logo from '../logo.svg';
+
+const Footer = () => {
+
+    return(
+        <div className="footer noselect">
+            <Link to="/">
+            <div className="logo">
+                <img src={logo} alt="" width="50px" />
+            </div>
+            </Link>
+        </div>
+    )
+};
+
+export default Footer;
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const Navigation = (props) => {
+    return(
+        <div className="navigation">
+            <ul className="right-nav">
+            <Link to= {props.links.href} className="nav-btn">
+                {props.links.title}
+            </Link>
+            </ul>
+        </div>
+    )
+};
+
+export default Navigation;
